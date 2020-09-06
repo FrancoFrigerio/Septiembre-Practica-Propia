54b6d7fbde8892263806a9406da066017d4dad63
FRANCO <FRANCO@Lenovo-PC>
2020-09-06 11:29:44 -0300

    Ultimod Cambios

---
+++ app-ws-soap-server/src/main/java/ar/com/EducacionIT/ws/soap/main/ProductoWSSoapMain.java
@@ -0,0 +1,13 @@
+package ar.com.EducacionIT.ws.soap.main;
+
+import javax.xml.ws.Endpoint;
+
+import ar.com.EducacionIT.ws.soap.server.ProductoWService;
+
+public class ProductoWSSoapMain{
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:8080/", new ProductoWService1|() {
+		
+		
+	}
+}
--- app-ws-soap-server/src/main/java/ar/com/EducacionIT/ws/soap/server/ProductoWService.java
+++ app-ws-soap-server/src/main/java/ar/com/EducacionIT/ws/soap/server/ProductoWService.java
@@ -1,5 +1,9 @@
 package ar.com.EducacionIT.ws.soap.server;
-
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+@WebService
 public interface ProductoWService {
+
 
 }
